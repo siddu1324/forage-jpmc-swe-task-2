071ae74e3e26b610125b25d678eb848e2e18cedf
spq6r <spq6r@umsl.edu>
2024-07-12 00:48:58 -0500

    fix graph.tsx

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -44,20 +44,17 @@ class Graph extends Component<IProps, {}> {
       this.table = window.perspective.worker().table(schema);
     }
     if (this.table) {
+      
       elem.load(this.table);
-      // Configure the perspective to view the data correctly
-      elem.setAttribute('view', 'y_line');
-      elem.setAttribute('column-pivots', JSON.stringify(['stock']));
-      elem.setAttribute('row-pivots', JSON.stringify(['timestamp']));
-      elem.setAttribute('columns', JSON.stringify(['top_ask_price']));
-      elem.setAttribute('aggregates', JSON.stringify({
-        stock: 'distinct count',
-        top_ask_price: 'avg',
-        top_bid_price: 'avg',
-        timestamp: 'distinct count',
-      }));
-    }
+      elem.setAttribute("view", "y_line");
+      elem.setAttribute("column-pivots", '["stock"]');
+      elem.setAttribute("row_pivots", '["timestamp"]');
+      elem.setAttribute("columns", '["top_ask_price"]');
+      elem.setAttribute("aggregates",
+          '{"stock":"distinct_count", "top_ask_price":"avg", "top_bid_price":"avg", "timestamp":"distinct_count"}'
+      );
   }
+}
   
 
   ccomponentDidUpdate() {
@@ -68,14 +65,10 @@ class Graph extends Component<IProps, {}> {
           top_ask_price: el.top_ask && el.top_ask.price || 0,
           top_bid_price: el.top_bid && el.top_bid.price || 0,
           timestamp: el.timestamp,
-        };
-      }).filter((el, index, self) => 
-        index === self.findIndex((t) => (
-          t.timestamp === el.timestamp && t.stock === el.stock
-        ))
-      ));
-    }
-  }  
+      };
+  }));
+}
+}
 }
 
 export default Graph;
