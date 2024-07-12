154fafd888af9ca7f88a8384634d295cc348de5a
spq6r <spq6r@umsl.edu>
2024-07-12 00:51:57 -0500

    fixed app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -60,27 +60,29 @@ class App extends Component<{}, IState> {
    */
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          Bank & Merge Co Task 2
-        </header>
-        <div className="App-content">
-          <button className="btn btn-primary Stream-button"
-            // when button is click, our react app tries to request
-            // new data from the server.
-            // As part of your task, update the getDataFromServer() function
-            // to keep requesting the data every 100ms until the app is closed
-            // or the server does not return anymore data.
-            onClick={() => {this.getDataFromServer()}}>
-            Start Streaming Data
-          </button>
-          <div className="Graph">
-            {this.renderGraph()}
-          </div>
+        <div className="App">
+            <header className="App-header">
+                Bank & Merge Co Task 2
+            </header>
+            <div className="App-content">
+                <button className="btn btn-primary Stream-button"
+                    // when button is click, our react app tries to request
+                    // new data from the server.
+                    // As part of your task, update the getDataFromServer() function
+                    // to keep requesting the data every 100ms until the app is closed
+                    // or the server does not return anymore data.
+                        onClick={() => {
+                            this.getDataFromServer()
+                        }}>
+                    Start Streaming Data
+                </button>
+                <div className="Graph">
+                    {this.renderGraph()}
+                </div>
+            </div>
         </div>
-      </div>
     )
-  }
+}
 }
 
 export default App;
